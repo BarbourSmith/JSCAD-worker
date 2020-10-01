bdb95f47613500aa35f83545e8e00122c499b213
BarbourSmith <BarbourSmith@users.noreply.github.com>
2020-10-01 10:12:24 -0700

    Add intersection and difference

--- src/worker.js
+++ src/worker.js
@@ -24,13 +24,13 @@ const {
 
 const { cuboid, sphere, cylinder, circle, star, rectangle } = require('@jscad/modeling').primitives
 const { translate, rotate, scale } = transforms
+const { union, subtract, intersect} = booleans
 const { colorize } = require('@jscad/modeling').colors
 const { extrudeLinear, extrudeRectangular, extrudeRotate } = require('@jscad/modeling').extrusions
 
 
 function circ(values){
 	var myCircle = colorize([1, 0, 0, 0.75],circle({ radius: values[0]}))
-	console.log(myCircle)
 	var serializedCircle = jsonSerializer.serialize({}, myCircle)
 	return serializedCircle
 }
@@ -54,12 +54,12 @@ function poly(values){
 function extr(values){
 
 	let geometry = values[0]
-	//var deserializedGeometry = jsonDeSerializer.deserialize({output: 'geometry'}, jsonGeometry)
-	const extrudedObj = extrudeLinear({height: values[1]}, geometry)
-	//translate([10, 5, 0], [jscad.primitives.cylinder({ radius: 0.5, segments: 64 })])
+	var deserializedGeometry = jsonDeSerializer.deserialize({output: 'geometry'}, geometry)
+	const extrudedObj = extrudeLinear({height: values[1]}, deserializedGeometry)
 	var serializedResult = jsonSerializer.serialize({}, extrudedObj)
 	return serializedResult
 }
+
 function rotat(values){
 	let geometry = values[0]
 	var deserializedGeometry = jsonDeSerializer.deserialize({output: 'geometry'}, geometry)
@@ -73,15 +73,32 @@ function trans(values){
 	let geometry = values[0]
 	var deserializedGeometry = jsonDeSerializer.deserialize({output: 'geometry'}, geometry)
 	const translatedObj = translate([values[1],values[2],values[3]], deserializedGeometry)
-	//translate([10, 5, 0], [jscad.primitives.cylinder({ radius: 0.5, segments: 64 })])
 	var serializedResult = jsonSerializer.serialize({}, translatedObj)
-	return serializedResult	
+	return serializedResult
+}
+
+function diff(values){
+	var deserializedGeometry0 = jsonDeSerializer.deserialize({output: 'geometry'}, values[0])
+	var deserializedGeometry1 = jsonDeSerializer.deserialize({output: 'geometry'}, values[1])
+	const subtractedObj = subtract(deserializedGeometry0, deserializedGeometry1)
+	var serializedResult = jsonSerializer.serialize({}, subtractedObj)
+	return serializedResult
+}
+
+function intersection(values){
+	var deserializedGeometry0 = jsonDeSerializer.deserialize({output: 'geometry'}, values[0])
+	var deserializedGeometry1 = jsonDeSerializer.deserialize({output: 'geometry'}, values[1])
+	const intersectionObj = intersect(deserializedGeometry0, deserializedGeometry1)
+	var serializedResult = jsonSerializer.serialize({}, intersectionObj)
+	return serializedResult
 }
 
 
 // create a worker and register public functions
 workerpool.worker({
  	circle: circ,
+    difference: diff,
+    intersection:intersection,
  	translate: trans,
  	rectangle: rect,
  	extrude: extr,
